a58d6b8de98d9745372a57c794d266c1514ee0f6
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-18 08:02:49 +0100

    Removed mock implementation and mocked values

--- src/infrastructure/inMemoryGitHubRepository.ts
+++
@@ -1,7 +0,0 @@
-import { githubApiResponses } from "./../github_api_responses";
-
-export class InMemoryGitHubRepositoryRepository {
-	search(): typeof githubApiResponses {
-		return githubApiResponses;
-	}
-}
