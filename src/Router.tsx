1045d83dcc525db4cc3a524e8c9184d4c80bf75b
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-23 07:06:02 +0100

    Added layout to browser router paths

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,15 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { DashboardFactory } from "./sections/dashboard/DashboardFactory";
 import { GitHubReposirotyDetail } from "./sections/detail/GitHubRepositoryDetail";
+import { Layout } from "./sections/layout/Layout";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: DashboardFactory.create(),
-	},
-	{
-		path: "/repository/:organization/:name",
-		element: <GitHubReposirotyDetail />,
+		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: DashboardFactory.create(),
+			},
+			{
+				path: "/repository/:organization/:name",
+				element: <GitHubReposirotyDetail />,
+			},
+		],
 	},
 ]);
 
