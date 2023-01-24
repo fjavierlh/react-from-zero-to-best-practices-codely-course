910af7b931f3fc843c2f5b6d39729c5ab592425f
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-24 19:07:46 +0100

    Added use memo to get widgets config

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
+import { useMemo } from "react";
+
 import { config } from "../../devdash_config";
 import { GitHubRepositoryRepository } from "../../domain/GitHubRepositoryRepository";
 import styles from "./Dashboard.module.scss";
 import { GitHubRepositoryWidget } from "./GitHubRepositoryWidget";
 import { useGitHubRepositories } from "./useGitHubRepositories";
 
-const gitHubRepositoriesUrls: string[] = config.widgets.map((widget) => widget.repository_url);
-
 export function Dashboard({ repository }: { repository: GitHubRepositoryRepository }) {
+	const gitHubRepositoriesUrls = useMemo(
+		() => config.widgets.map((widget) => widget.repository_url),
+		[]
+	);
 	const { gitHubRepositories } = useGitHubRepositories(repository, gitHubRepositoriesUrls);
 
 	return (
