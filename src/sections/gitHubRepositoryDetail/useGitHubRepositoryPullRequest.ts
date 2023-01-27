5f7f2336c8680839aee07a7da6c8f8b1378d2937
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-27 07:48:04 +0100

    Created git hub repository pull requests hook

---
+++ src/sections/gitHubRepositoryDetail/useGitHubRepositoryPullRequest.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+import { RepositoryId } from "../../domain/GitHubRepository";
+import { GitHubRepositoryPullRequest } from "../../domain/GitHubRepositoryPullRequest";
+import { GitHubRepositoryPullRequestRepository } from "./../../domain/GitHubRepositoryPullRequestRepository";
+
+export function useGitHubRepositoryPullRequest(
+	repository: GitHubRepositoryPullRequestRepository,
+	respositoryId: RepositoryId
+): { pullRequests: GitHubRepositoryPullRequest[]; isLoading: boolean } {
+	const [pullRequests, setPullRequests] = useState<GitHubRepositoryPullRequest[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		repository
+			.search(respositoryId)
+			.then((pullRequests) => {
+				setPullRequests(pullRequests);
+				setIsLoading(false);
+			})
+			.catch((error) => console.error(error));
+	}, [repository, respositoryId]);
+
+	return { pullRequests, isLoading };
+}
