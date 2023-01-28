7827db15429e4bd0fca432c7207ce27525f02e25
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-28 10:25:08 +0100

    Updated dashboard test

--- tests/Dashboard.test.tsx
+++ tests/Dashboard.test.tsx
@@ -1,20 +1,27 @@
 import { screen } from "@testing-library/react";
 import { mock } from "jest-mock-extended";
 
+import { RepositoryWidgetRepository } from "../src/domain/RepositoryWidgetRepository";
 import { Dashboard } from "../src/sections/dashboard/Dashboard";
 import { GitHubRepositoryRepository } from "./../src/domain/GitHubRepositoryRepository";
 import { GitHubRepositoryMother } from "./GitHubRepositoryMother";
 import { renderWithRouter } from "./renderWithRouter";
 
-const mockReposiroty = mock<GitHubRepositoryRepository>();
+const mockGitHubRepositoryRepository = mock<GitHubRepositoryRepository>();
+const mockRepositoryWidgetRepository = mock<RepositoryWidgetRepository>();
 
 describe("Dashboard", () => {
 	const gitHubRepository = GitHubRepositoryMother.create();
 
 	it("show all widgets", async () => {
-		mockReposiroty.search.mockResolvedValue([gitHubRepository]);
+		mockGitHubRepositoryRepository.search.mockResolvedValue([gitHubRepository]);
 
-		renderWithRouter(<Dashboard repository={mockReposiroty} />);
+		renderWithRouter(
+			<Dashboard
+				gitHubRepositoryRepository={mockGitHubRepositoryRepository}
+				repositoryWidgetRepository={mockRepositoryWidgetRepository}
+			/>
+		);
 
 		const firstWidgetTitle = `${gitHubRepository.id.organization}/${gitHubRepository.id.name}`;
 		const firstWidgetHeader = await screen.findByRole("heading", {
@@ -25,9 +32,14 @@ describe("Dashboard", () => {
 	});
 
 	it("show not results message when there are no widgets", async () => {
-		mockReposiroty.search.mockResolvedValue([]);
+		mockGitHubRepositoryRepository.search.mockResolvedValue([]);
 
-		renderWithRouter(<Dashboard repository={mockReposiroty} />);
+		renderWithRouter(
+			<Dashboard
+				gitHubRepositoryRepository={mockGitHubRepositoryRepository}
+				repositoryWidgetRepository={mockRepositoryWidgetRepository}
+			/>
+		);
 
 		const emptyMessage = await screen.findByText(new RegExp("No hay widgets configurados", "i"));
 		expect(emptyMessage).toBeInTheDocument();
@@ -36,9 +48,14 @@ describe("Dashboard", () => {
 	it("show last modified date in human readable format", async () => {
 		const gitHubRepository = GitHubRepositoryMother.create({ updatedAt: new Date() });
 
-		mockReposiroty.search.mockResolvedValue([gitHubRepository]);
+		mockGitHubRepositoryRepository.search.mockResolvedValue([gitHubRepository]);
 
-		renderWithRouter(<Dashboard repository={mockReposiroty} />);
+		renderWithRouter(
+			<Dashboard
+				gitHubRepositoryRepository={mockGitHubRepositoryRepository}
+				repositoryWidgetRepository={mockRepositoryWidgetRepository}
+			/>
+		);
 
 		const modificationDate = await screen.findByText(new RegExp("today", "i"));
 
