5456a29e5d716b950f113d0ef22f1b8501f823bd
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-28 08:24:08 +0100

    Integrated pull requests in detail component

--- src/sections/gitHubRepositoryDetail/GitHubRepositoryDetail.tsx
+++ src/sections/gitHubRepositoryDetail/GitHubRepositoryDetail.tsx
@@ -1,19 +1,32 @@
 import { useMemo } from "react";
 import { useParams } from "react-router-dom";
 
+import { GitHubRepositoryPullRequestRepository } from "../../domain/GitHubRepositoryPullRequestRepository";
 import { GitHubRepositoryRepository } from "../../domain/GitHubRepositoryRepository";
 import { ReactComponent as Lock } from "./../../assets/svgs/lock.svg";
 import { ReactComponent as Unlock } from "./../../assets/svgs/unlock.svg";
 import { DetailSkeleton } from "./DetailSkeleton";
 import styles from "./GitHubRepositoryDetail.module.scss";
+import { PullRequests } from "./PullRequests";
 import { useGitHubRepository } from "./useGItHubRepository";
+import { useInViewport } from "./useInViewport";
 
-export function GitHubRepositoryDetail({ repository }: { repository: GitHubRepositoryRepository }) {
+export function GitHubRepositoryDetail({
+	gitHubRepositoryRepository,
+	gitHubRepositoryPullRequestsRepository,
+}: {
+	gitHubRepositoryRepository: GitHubRepositoryRepository;
+	gitHubRepositoryPullRequestsRepository: GitHubRepositoryPullRequestRepository;
+}) {
+	const { isInViewport, ref } = useInViewport();
 	const { organization, name } = useParams() as { organization: string; name: string };
 
 	const repositoryId = useMemo(() => ({ name, organization }), [name, organization]);
 
-	const { gitHubRepository, isLoading } = useGitHubRepository(repository, repositoryId);
+	const { gitHubRepository, isLoading } = useGitHubRepository(
+		gitHubRepositoryRepository,
+		repositoryId
+	);
 
 	if (isLoading) {
 		return <DetailSkeleton />;
@@ -60,36 +73,51 @@ export function GitHubRepositoryDetail({ repository }: { repository: GitHubRepos
 			</table>
 
 			<h3>Workflow runs status</h3>
-			<p>
-				⏱️Last workflow run:{" "}
-				{gitHubRepository.workflowRunsStatus[0].createdAt.toLocaleDateString("es-ES")}
-			</p>
-			<table className={styles.detail__table}>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Title</th>
-						<th>Date</th>
-						<th>Status</th>
-						<th>Conclusion</th>
-					</tr>
-				</thead>
-				<tbody>
-					{gitHubRepository.workflowRunsStatus.map((run) => (
-						<tr key={run.id}>
-							<td>{run.name}</td>
-							<td>
-								<a href={run.url} target="_blank" rel="noreferrer">
-									{run.title}
-								</a>
-							</td>
-							<td>{run.createdAt.toLocaleDateString("es-ES")}</td>
-							<td>{run.status}</td>
-							<td>{run.conclusion}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			{gitHubRepository.workflowRunsStatus.length > 0 ? (
+				<>
+					<p>
+						⏱️Last workflow run:{" "}
+						{gitHubRepository.workflowRunsStatus[0].createdAt.toLocaleDateString("es-ES")}
+					</p>
+					<table className={styles.detail__table}>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Title</th>
+								<th>Date</th>
+								<th>Status</th>
+								<th>Conclusion</th>
+							</tr>
+						</thead>
+						<tbody>
+							{gitHubRepository.workflowRunsStatus.map((run) => (
+								<tr key={run.id}>
+									<td>{run.name}</td>
+									<td>
+										<a href={run.url} target="_blank" rel="noreferrer">
+											{run.title}
+										</a>
+									</td>
+									<td>{run.createdAt.toLocaleDateString("es-ES")}</td>
+									<td>{run.status}</td>
+									<td>{run.conclusion}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</>
+			) : (
+				<>
+					<section ref={ref}>
+						{isInViewport && (
+							<PullRequests
+								repository={gitHubRepositoryPullRequestsRepository}
+								repositoryId={repositoryId}
+							/>
+						)}
+					</section>
+				</>
+			)}
 		</section>
 	);
 }
