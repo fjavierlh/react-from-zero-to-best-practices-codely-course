24c77960fb13c69546fb202e19fd990f15f556df
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-30 08:20:19 +0100

    Implemented error in add repository widget hook

--- src/sections/dashboard/useAddRepositoryWidget.ts
+++ src/sections/dashboard/useAddRepositoryWidget.ts
@@ -1,11 +1,20 @@
+import { RepositoryAlreadyExistsError } from "../../domain/RepositoryAlreadyExistsError";
 import { RepositoryWidget } from "./../../domain/RepositoryWidget";
 import { RepositoryWidgetRepository } from "./../../domain/RepositoryWidgetRepository";
 
 export function useAddRepositoryWidget(repository: RepositoryWidgetRepository): {
-	add: (repositoryWidget: RepositoryWidget) => Promise<void>;
+	add: (repositoryWidget: RepositoryWidget) => Promise<RepositoryAlreadyExistsError | void>;
 } {
 	return {
-		add: async (repositoryWidget: RepositoryWidget) => {
+		add: async (
+			repositoryWidget: RepositoryWidget
+		): Promise<RepositoryAlreadyExistsError | void> => {
+			const widgetRepositories = await repository.search();
+
+			if (widgetRepositories.some((widget) => widget.url === repositoryWidget.url)) {
+				return new RepositoryAlreadyExistsError(repositoryWidget.url);
+			}
+
 			await repository.persist(repositoryWidget);
 		},
 	};
