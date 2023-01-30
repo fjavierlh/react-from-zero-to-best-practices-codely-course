297a3806852b4827968b3df35af65552362d8c83
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-30 08:16:59 +0100

    Added new test case to form  when repository already exists

--- tests/sections/dashboard/AddRepositoryWidgetForm.test.tsx
+++ tests/sections/dashboard/AddRepositoryWidgetForm.test.tsx
@@ -22,6 +22,7 @@ describe("AddRepositoryWidgetForm", () => {
 	});
 
 	test("add new widget when form is submitted", async () => {
+		mockRepositoryWidget.search.mockResolvedValue([]);
 		const newRepositoryWidget: RepositoryWidget = {
 			id: "someId",
 			url: "https://github.com/some-repo",
@@ -51,5 +52,39 @@ describe("AddRepositoryWidgetForm", () => {
 		expect(addAnotherNewWidgetButton).toBeInTheDocument();
 		// eslint-disable-next-line @typescript-eslint/unbound-method
 		expect(mockRepositoryWidget.persist).toHaveBeenCalledWith(newRepositoryWidget);
+
+		mockRepositoryWidget.persist.mockReset();
+	});
+
+	test("show error when repository already exists in dashboard", async () => {
+		const existingRepositoryWidget: RepositoryWidget = {
+			id: "existingRepositoryWidgetId",
+			url: "https://github.com/existing-repo",
+		};
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+		mockRepositoryWidget.search.mockResolvedValue([existingRepositoryWidget]);
+
+		render(<AddRepositoryWidgetForm repositoryWidget={mockRepositoryWidget} />);
+
+		const addWidgetButton = await screen.findByRole("button", { name: /Añadir repositorio/i });
+
+		userEvent.click(addWidgetButton);
+
+		const idInput = screen.getByLabelText(/Id/i);
+		userEvent.type(idInput, existingRepositoryWidget.id);
+
+		const urlInput = screen.getByLabelText(/Url del repositorio/i);
+		userEvent.type(urlInput, existingRepositoryWidget.url);
+
+		const submitButton = await screen.findByRole("button", { name: /Añadir/i });
+		userEvent.click(submitButton);
+
+		const errorMessage = await screen.findByRole("alert", {
+			description: /Repositorio duplicado/i,
+		});
+
+		expect(errorMessage).toBeInTheDocument();
+		// eslint-disable-next-line @typescript-eslint/unbound-method
+		expect(mockRepositoryWidget.persist).not.toHaveBeenCalled();
 	});
 });
