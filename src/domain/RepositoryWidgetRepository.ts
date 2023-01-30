577dd1c09b5c5a6a1ffbb92e7566584386cc2beb
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-30 07:14:04 +0100

    Added search method to interface

--- src/domain/RepositoryWidgetRepository.ts
+++ src/domain/RepositoryWidgetRepository.ts
@@ -1,5 +1,7 @@
+import { RepositoryId } from "./GitHubRepository";
 import { RepositoryWidget } from "./RepositoryWidget";
 
 export interface RepositoryWidgetRepository {
 	persist(repositoryWidget: RepositoryWidget): Promise<void>;
+	search(repositoryId: RepositoryId): Promise<RepositoryWidget[]>;
 }
