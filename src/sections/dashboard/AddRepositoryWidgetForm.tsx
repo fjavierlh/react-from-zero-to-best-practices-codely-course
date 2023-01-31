90cdec0b07eae7df20e9d72548e4d5df07e09efe
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2023-01-31 08:18:22 +0100

    Refactored not valid url use case

--- src/sections/dashboard/AddRepositoryWidgetForm.tsx
+++ src/sections/dashboard/AddRepositoryWidgetForm.tsx
@@ -1,11 +1,9 @@
 import { FormEvent, useState } from "react";
 
 import { ReactComponent as Add } from "../../assets/svgs/add.svg";
-import { RepositoryAlreadyExistsError } from "../../domain/RepositoryAlreadyExistsError";
-import { RepositoryURLisNotValidError } from "../../domain/RepositoryURLisNotValidError";
 import { RepositoryWidgetRepository } from "../../domain/RepositoryWidgetRepository";
 import styles from "./AddRepositoryWidgetForm.module.scss";
-import { useAddRepositoryWidget } from "./useAddRepositoryWidget";
+import { AddRepositoryWidgetErrors, useAddRepositoryWidget } from "./useAddRepositoryWidget";
 
 type FormFields = { id: string; url: string };
 
@@ -19,23 +17,19 @@ export function AddRepositoryWidgetForm({
 	repositoryWidget: RepositoryWidgetRepository;
 }) {
 	const [isFormActive, setIsFormActive] = useState(false);
-	const [hasAlreadyExistsError, setHasAlreadyExistsError] = useState(false);
-	const [isNotValidURLError, setIsNotValidURLError] = useState(false);
+	const [formHasErrors, setFormHasErrors] = useState<AddRepositoryWidgetErrors | null>();
 	const { add } = useAddRepositoryWidget(repositoryWidget);
 
 	const submitForm = async (event: GenericFormEvent<FormFields>) => {
 		event.preventDefault();
 		const { id, url } = event.target.elements;
-		const error = await add({ id: id.value, url: url.value });
+		const errors = await add({
+			id: id.value,
+			url: url.value,
+		});
 
-		if (error instanceof RepositoryURLisNotValidError) {
-			setIsNotValidURLError(true);
-
-			return;
-		}
-
-		if (error instanceof RepositoryAlreadyExistsError) {
-			setHasAlreadyExistsError(true);
+		if (errors) {
+			setFormHasErrors(errors);
 
 			return;
 		}
@@ -46,7 +40,7 @@ export function AddRepositoryWidgetForm({
 	return (
 		<article className={styles.add_widget}>
 			<div className={styles.container}>
-				{!isFormActive && !hasAlreadyExistsError ? (
+				{!isFormActive && !formHasErrors ? (
 					<button onClick={() => setIsFormActive(true)} className={styles.add_button}>
 						<Add />
 						<p>Añadir repositorio</p>
@@ -66,12 +60,12 @@ export function AddRepositoryWidgetForm({
 						<div>
 							<input type="submit" value={"Añadir"} />
 						</div>
-						{hasAlreadyExistsError && (
+						{formHasErrors?.hasAlreadyExistsError && (
 							<p className={styles.error} role="alert" aria-describedby="duplicated-error">
 								<span id="duplicated-error">Repositorio duplicado</span>
 							</p>
 						)}
-						{isNotValidURLError && (
+						{formHasErrors?.isNotValidURLError && (
 							<p className={styles.error} role="alert" aria-describedby="not-valid-url-error">
 								<span id="not-valid-url-error">Introduce una url válida</span>
 							</p>
--- src/sections/dashboard/useAddRepositoryWidget.ts
+++ src/sections/dashboard/useAddRepositoryWidget.ts
@@ -3,26 +3,35 @@ import { RepositoryURLisNotValidError } from "../../domain/RepositoryURLisNotVal
 import { RepositoryWidget } from "./../../domain/RepositoryWidget";
 import { RepositoryWidgetRepository } from "./../../domain/RepositoryWidgetRepository";
 
+export type AddRepositoryWidgetErrors = {
+	hasAlreadyExistsError?: boolean;
+	isNotValidURLError?: boolean;
+};
+
 export function useAddRepositoryWidget(repository: RepositoryWidgetRepository): {
-	add: (repositoryWidget: RepositoryWidget) => Promise<RepositoryAlreadyExistsError | void>;
+	add: (repositoryWidget: RepositoryWidget) => Promise<AddRepositoryWidgetErrors | null>;
 } {
 	return {
-		add: async (
-			repositoryWidget: RepositoryWidget
-		): Promise<RepositoryAlreadyExistsError | RepositoryURLisNotValidError | void> => {
+		add: async (repositoryWidget: RepositoryWidget): Promise<AddRepositoryWidgetErrors | null> => {
 			try {
 				new URL(repositoryWidget.url);
 			} catch {
-				return new RepositoryURLisNotValidError(repositoryWidget.url);
+				return {
+					isNotValidURLError: !!new RepositoryURLisNotValidError(repositoryWidget.url),
+				};
 			}
 
 			const widgetRepositories = await repository.search();
 
 			if (widgetRepositories.some((widget) => widget.url === repositoryWidget.url)) {
-				return new RepositoryAlreadyExistsError(repositoryWidget.url);
+				return {
+					hasAlreadyExistsError: !!new RepositoryAlreadyExistsError(repositoryWidget.url),
+				};
 			}
 
 			await repository.persist(repositoryWidget);
+
+			return null;
 		},
 	};
 }
